5e73e90aadb2058edf2db241d1c60b97a890b923
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-02-16 23:59:15 +0500

    added project module1

---
+++ android-voice-recorder-master/app/src/main/java/com/ubit/VoiceSearchActivity.java
@@ -0,0 +1,15 @@
+package com.ubit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ubit.voicecontentsearcher.R;
+
+public class VoiceSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_search);
+    }
+}
